17d379855a0d8e5eecf437956f2106f513982ef3
Drew Pham <drewvpham@gmail.com>
2016-12-05 16:07:19 +0000

    created files and folder structure

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/austins_best_pizza/scraper'
+require_relative '../lib/austins_best_pizza/restaurant'
+require_relative '../lib/austins_best_pizza/cli'
